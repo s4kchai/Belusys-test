4069f081aa2831c736f6a7aeafac6bb5013a7259
SAKCHAI JOMBOON <sakchai.jomboon@gmail.com>
2025-09-24 17:25:44 +0700

    fixing config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import * as express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
 
+  app.use(express.static(join(__dirname, '../../frontend/dist')));
+  app.use((req, res, next) => {
+    if (!req.path.startsWith('/api')) {
+      res.sendFile(join(__dirname, '../../frontend/dist/index.html'));
+    } else {
+      next();
+    }
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
